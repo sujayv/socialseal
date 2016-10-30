78da279a54424113586078900c518b985cc65778
sujayv <sujay.vaishampayan@gmail.com>
2016-10-30 14:51:34 -0700

    Author details extracted from 0day.com

--- crawler.py
+++ crawler.py
@@ -3,32 +3,76 @@ from ip_renew import *
 from bs4 import BeautifulSoup
 import re
 
-for i in range(0,1):
-    renew_connection()
-    #print request("http://icanhazip.com/")
-    #print request("http://xmh57jrzrnw6insl.onion")
-    #print request("http://2222ppclgy2amp23.onion/")
-    #url = "http://xmh57jrzrnw6insl.onion"
-    url2 = "http://rrcc5uuudhh4oz3c.onion/"
-    url = "http://rrcc5uuudhh4oz3c.onion/?cmd=category&id=12"
-    #print request(url)
-    r = request(url)
-    soup = BeautifulSoup(r)
-    #print soup.prettify()
-    #found = soup.findAll("a",{"style": "font-weight:bold;"})[0].string
-    for link in soup.findAll('a', href=True):
-        print "Found URL is: %s"%link['href']
-        print "For topic %s"%link.string
-        #print link.string
-    #found2 = soup.findAll("a",{"style": "font-weight:bold;"})[0].string
-    #print found
-
-
-'''def make_request():
-    r = request(url)
-    data = r.text
-    soup = BeautifulSoup(data)
-    for link in soup.find_all('a'):
-        print(link.get('href'))
-
-make_request'''
+renew_connection()
+#print request("http://icanhazip.com/")
+#print request("http://xmh57jrzrnw6insl.onion")
+#print request("http://2222ppclgy2amp23.onion/")
+#url = "http://xmh57jrzrnw6insl.onion"
+url = "http://mvfjfugdwgc5uwho.onion/author/"
+#url2 = "http://rrcc5uuudhh4oz3c.onion/"
+#url = "http://rrcc5uuudhh4oz3c.onion/?cmd=category&id=12"
+#print request(url)
+#print website.read()
+#print website.read()
+#print website.geturl()
+#cookie = website.info()['Set-Cookie']
+#index = cookie.index(';')
+#print cookie[0:index]
+#dict = {'agree':'Yes, I agree'}
+#encoded = urllib.urlencode(dict)<a href="/author/0">
+#print url+'?'+encoded
+authornodes = {}
+for i in range(1,10):
+	forum = requestpost(url+(str)(i),"")
+	#forum = requestget(url)
+	soup = BeautifulSoup(forum.read(),'lxml')
+	#print soup
+	####For author name######
+	authornametag = soup.find_all('div',class_='category_title')
+	#r = re.compile("Author: ")
+	#print authornametag
+	m = re.search('Author: (.*)<\/a>',str(authornametag))
+	authorurl = m.group(0)
+	print "\n"
+	#print authorurl
+	anchortagindex = authorurl.index("</a>")
+	authorname = authorurl[8:anchortagindex]
+	#print authorname
+	######For author details
+	authordetails = soup.find_all('div',class_='user_table_content')
+	table = {}
+	for details in authordetails:
+		#print details
+		detailsoup = BeautifulSoup("<html>"+str(details)+"</html>","lxml")
+		#print detailsoup
+		field = detailsoup.find_all('div',class_='td')
+		print field[0].string
+		if(field[0].string == 'Author'):
+			tp = detailsoup.find_all('a')
+			print tp[0].string
+			node = {field[0].string:tp[0].string}
+			table.update(node)
+		else:
+			print field[1].string
+			node = {field[0].string : field[1].string}
+			table.update(node)
+	finaltable = {i:table}
+	authornodes.update(finaltable)
+for key in authornodes.keys():
+	print "***********************"
+	temp = authornodes.get(key)
+	print 'Author '+ str(key)
+	for value in temp.keys():
+		print value + " : \t" + str(temp.get(value))
+	#print soup.title.string
+	#all_links=soup.find_all('a')
+	#for link in all_links:
+		#print link.get("href")
+#print website.header_items()
+#found = soup.findAll("a",{"style": "font-weight:bold;"})[0].string
+#for link in soup.findAll('a', href=True):
+#    #print "Found URL is: %s"%link['href']
+#    print "For topic %s"%link.string
+    #print link.string
+#found2 = soup.findAll("a",{"style": "font-weight:bold;"})[0].string
+#print found
--- ip_renew.py
+++ ip_renew.py
@@ -1,18 +1,42 @@
 #!/usr/bin/python
 from TorCtl import TorCtl
 import urllib2
+import urllib
 
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+#headers={'User-Agent':user_agent,'Cookie':'PHPSESSID=377nje1uil206okervak02kti7; __utma=157815014.692379856.1476755824.1476755824.1476755824.1; __utmb=157815014.3.10.1476755824; __utmc=157815014; __utmz=157815014.1476755824.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'}
 headers={'User-Agent':user_agent}
 
-def request(url):
-    def _set_urlproxy():
-        proxy_support = urllib2.ProxyHandler({"http" : "127.0.0.1:8118"})
-        opener = urllib2.build_opener(proxy_support)
-        urllib2.install_opener(opener)
+
+def _set_urlproxy():
+    proxy_support = urllib2.ProxyHandler({"http" : "127.0.0.1:8118"})
+    opener = urllib2.build_opener(proxy_support)
+    urllib2.install_opener(opener)
+
+def requestget(url):
     _set_urlproxy()
-    request=urllib2.Request(url, None, headers)
-    return urllib2.urlopen(request).read()
+    requesturl=urllib2.Request(url, None, headers)
+    return urllib2.urlopen(requesturl)
+
+def requestpost(url,cookie):
+    _set_urlproxy()
+    #headernew = {'Cookie':cookie,'Content-Type':'application/x-www-form-urlencoded','Content-Length':20}
+    headernew = {'Cookie':'PHPSESSID=7l9a4pj2edeac5u0eh33tcc081'}
+    headernew.update(headers)
+    #print headernew
+    #dict = {'agree':'Yes, I agree'}
+    #encoded = urllib.urlencode(dict)
+    #print encoded
+    requesturl=urllib2.Request(url,None,headernew)
+    return urllib2.urlopen(requesturl)
+
+def requestgetnew(url,cookie):
+    _set_urlproxy()
+    headernew = {'Cookie':cookie}
+    headernew.update(headers)
+    #print headernew
+    requesturl=urllib2.Request(url, "", headernew)
+    return urllib2.urlopen(requesturl)
 
 def renew_connection():
     conn = TorCtl.connect(controlAddr="127.0.0.1", controlPort=9051, passphrase="your_password")
